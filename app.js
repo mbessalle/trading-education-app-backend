7b9520f19d294b1fac140a18d2086fcab3d65852
Moises Bessalle <mbessalle@gmail.com>
2020-07-06 19:29:39 +0200

    fixed CORS issue

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const corsMiddleWare = require('cors');
 
 const coursesRouter = require('./routes/courses');
 const usersRouter = require('./routes/users');
@@ -13,6 +14,7 @@ const app = express();
 // view engine setup.
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(corsMiddleWare());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
